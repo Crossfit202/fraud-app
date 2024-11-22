73e976e44a2a6a643a74efd1f0750d4ec4d8c5df
Joncross202 <cro21035@byui.edu>
2024-11-22 13:42:41 -0500

    created docker files/ made .env for db login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuditLogModule } from './audit_log/audit_log.module';
@@ -6,39 +8,52 @@ import { ReportAnnotationsModule } from './report_annotations/report_annotations
 import { ReportStatusHistoryModule } from './report_status_history/report_status_history.module';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Audit_log } from './audit_log/audit_log';
 import { Report_annotations } from './report_annotations/report_annotations';
 import { Report_status_history } from './report_status_history/report_status_history';
 import { Reports } from './reports/reports';
 import { Users } from './users/users';
-import { AuditLogController } from './audit_log/audit_log.controller';
-import { ReportAnnotationsController } from './report_annotations/report_annotations.controller';
-import { ReportStatusHistoryController } from './report_status_history/report_status_history.controller';
-import { ReportsController } from './reports/reports.controller';
-import { UsersController } from './users/users.controller';
-import { AuditLogService } from './audit_log/audit_log.service';
-import { ReportAnnotationsService } from './report_annotations/report_annotations.service';
-import { ReportStatusHistoryService } from './report_status_history/report_status_history.service';
-import { ReportsService } from './reports/reports.service';
-import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'quokka-db-instance-1.cls8gcae0v9f.us-east-1.rds.amazonaws.com',
-      port: 5432,
-      username: 'postgres',
-      password: 'Cro$$57jon',
-      database: 'postgres',
-      autoLoadEntities: true,
-      entities: [Audit_log, Report_annotations, Report_status_history, Reports, Users],
-      synchronize: true
+
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
     }),
 
-    AuditLogModule, ReportAnnotationsModule, ReportStatusHistoryModule, ReportsModule, UsersModule],
-  controllers: [AppController, AuditLogController, ReportAnnotationsController, ReportStatusHistoryController, ReportsController, UsersController],
-  providers: [AppService, AuditLogService, ReportAnnotationsService, ReportStatusHistoryService, ReportsService, UsersService],
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT'), 10),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        autoLoadEntities: true,
+        entities: [Audit_log, Report_annotations, Report_status_history, Reports, Users],
+        synchronize: true,
+      }),
+    }),
+
+
+    AuditLogModule,
+    ReportAnnotationsModule,
+    ReportStatusHistoryModule,
+    ReportsModule,
+    UsersModule,
+  ],
+  controllers: [
+    AppController,
+  ],
+  providers: [
+    AppService,
+  ],
 })
 export class AppModule { }
